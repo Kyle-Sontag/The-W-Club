e482662152fbf407f3ab8b473490e871db5ac219
Kyle Sontag <ksontag70@rrc.ca>
2025-11-23 22:11:33 -0600

    Updated products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,9 @@
 class Product < ApplicationRecord
   belongs_to :category
+  has_many :order_items
+  has_many :orders, through: :order_items
 
   validates :name, presence: true
-  validates :description, presence: true, allow_blank: true
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :category, presence: true
 end
