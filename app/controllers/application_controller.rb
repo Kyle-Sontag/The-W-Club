e442a32e273de343b03e7cb8d2ec25e7c332daa2
Kyle Sontag <ksontag70@rrc.ca>
2025-11-30 01:29:52 -0600

    Fixed cart logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,12 +13,49 @@ class ApplicationController < ActionController::Base
 
   def set_cart
     if user_signed_in?
-      @current_cart = current_user.cart || Cart.create(user: current_user)
+      # Check if there's a guest cart in session
+      if session[:cart_id]
+        guest_cart = Cart.find_by(id: session[:cart_id], user_id: nil)
+
+        if guest_cart
+          # Merge guest cart into user's cart
+          user_cart = current_user.cart || current_user.create_cart
+
+          guest_cart.cart_items.each do |guest_item|
+            existing_item = user_cart.cart_items.find_by(product_id: guest_item.product_id)
+
+            if existing_item
+              existing_item.quantity += guest_item.quantity
+              existing_item.save
+            else
+              user_cart.cart_items.create(
+                product: guest_item.product,
+                quantity: guest_item.quantity
+              )
+            end
+          end
+
+          # Delete guest cart and clear session
+          guest_cart.destroy
+          session.delete(:cart_id)
+        end
+      end
+
+      @current_cart = current_user.cart || current_user.create_cart
     else
       # For guest users, use session-based cart
-      session[:cart_id] ||= Cart.create.id
-      @current_cart = Cart.find_by(id: session[:cart_id])
-      @current_cart ||= Cart.create.tap { |cart| session[:cart_id] = cart.id }
+      if session[:cart_id]
+        @current_cart = Cart.find_by(id: session[:cart_id])
+      end
+
+      unless @current_cart
+        @current_cart = Cart.create
+        session[:cart_id] = @current_cart.id
+      end
     end
+  rescue ActiveRecord::RecordInvalid
+    # If cart creation fails, create a new one
+    @current_cart = Cart.create
+    session[:cart_id] = @current_cart.id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_30_031229) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_30_072650) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -91,7 +91,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_30_031229) do
   end
 
   create_table "carts", force: :cascade do |t|
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_carts_on_user_id"
